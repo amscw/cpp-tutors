7349117dac0a0359e5d44f48869a8e627dd652a2
Alex M <moskwin.a.va@gmail.com>
2020-04-03 23:01:38 +0300

    add compile-time check

--- ex05-pinmerge/src/pinmerge.cpp
+++ ex05-pinmerge/src/pinmerge.cpp
@@ -8,9 +8,6 @@
 
 #include <iostream>
 
-// пакет параметров шаблона может содержать произвольные типы
-// variadic-функция
-// 1 инстанс шаблонной функции: функция вызывает сама себя, передавая себе на один аргумент меньше, пока не дойдет до финальной перегрузки
 class Test
 {
 
@@ -26,19 +23,14 @@ public:
 
 	static pinmask_t PinMerge(pinmask_t pins)
 	{
-		// конец рекурсии
 		return pins;
 	}
 };
 
-// пакет параметров шаблона содержит определенный тип
-// variadic-шаблон
-// число инстансов функции равно числу аргументов - 1: функция вызывает другую функцию, инстанцированную с меньшим на 1 числом параметров шаблона
 /*
- * Пусть, объединяем пины 5,4,3,2. Получаем цепочку инстансов
  * PinMerge<5,4,3,2>(pinnum_t pins) {pins |= 1 << 5; PinMerge<4,3,2>(pins); }
  * PinMerge<4,3,2>(pinnum_t pins) 	{pins |= 1 << 4; PinMerge<3,2>(pins); }
- * PinMerge<3,2>(pinnum_t pins)		{pins |= 1 << 3; PinMerge<2>(pins); }	// явно прописана специализация
+ * PinMerge<3,2>(pinnum_t pins)		{pins |= 1 << 3; PinMerge<2>(pins); }
  * PinMerge<2>(pinnum_t pins)		{pins |= 1 << 2; return pins; }
  */
 class Test2
@@ -47,13 +39,15 @@ public:
 	typedef std::uint8_t pinnum_t;
 	typedef std::uint32_t pinmask_t;
 
-	template<pinnum_t first, pinnum_t next, pinnum_t... rest> static pinmask_t PinMerge(pinmask_t pins)
+	template<pinnum_t first, pinnum_t next, pinnum_t... rest>
+	static constexpr pinmask_t PinMerge(pinmask_t pins)
 	{
 		pins |= 1 << first;
 		return PinMerge<next, rest...>(pins);
 	}
 
-	template<pinnum_t last> static pinmask_t PinMerge(pinmask_t pins)
+	template<pinnum_t last>
+	static constexpr pinmask_t PinMerge(pinmask_t pins)
 	{
 		return pins |= 1 << last;
 	}
@@ -67,10 +61,11 @@ int main() {
 	pins = Test::PinMerge(pins, 5, 4, 3, 2);
 	std::cout << "Test: pins = 0x" << std::hex << pins << std::endl;
 
-	// Test2
-	pins = 0;
-	pins = Test2::PinMerge<5,4,3,2>(pins);
-	std::cout << "Test2: pins = 0x" << std::hex << pins << std::endl;
+	// Test2 - check for comile-time calculations
+	// if it is not a const, - it will be compile error
+	static std::uint8_t testArray[Test2::PinMerge<3,2>(0)];
+	std::cout << "sizeof testArray: " <<  sizeof testArray << std::endl;
+
 
 
 	return 0;
